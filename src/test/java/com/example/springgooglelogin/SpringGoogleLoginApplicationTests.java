c4600db821e10e85a1d3c6150c01ecdc98ab27be
uojin29 <uojin29@gmail.com>
2024-02-12 19:52:14 +0900

    git init

---
+++ src/test/java/com/example/springgooglelogin/SpringGoogleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgooglelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGoogleLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
